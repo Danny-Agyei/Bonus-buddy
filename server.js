289dc4f2413420eb87fab5847f65272572f6a512
Daniel Agyei <dandesign96@gmail.com>
2024-01-03 21:47:39 +0000

    fix the form issue

--- server.js
+++ server.js
@@ -478,8 +478,8 @@ app.get("*", (req, res) => {
 //SET DEFAULT PORT IF IN DEVELOPMENT MODE
 const PORT = process.env.PORT || 5000;
 
-// connectDB().then(() => {
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT} in ${process.env.NODE_ENV} mode`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT} in ${process.env.NODE_ENV} mode`);
+  });
 });
-// });
