2aae09a685c284ab883818c86db58c2f230a3015
Danny-Agyei <87632293+Danny-Agyei@users.noreply.github.com>
2024-03-02 05:13:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ import mongoose from "mongoose";
 import cors from "cors";
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 //Models & DBs
