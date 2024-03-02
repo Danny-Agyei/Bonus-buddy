cf9e49ed0eb6058de51f27bec7473dcb8cbbe04f
Danny-Agyei <87632293+Danny-Agyei@users.noreply.github.com>
2024-03-02 05:10:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import md5 from "md5";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-const path = require('path');
+import path from 'path';
 
 const app = express();
 
