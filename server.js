6eca0e549c24a9f74b87171c8c1db11581b5b46d
Danny Code Media <87632293+Danny-Agyei@users.noreply.github.com>
2024-06-01 15:18:08 +0000

    Update server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // });
 
 //Handle webhook request
-app.post("/", async (req, res, next) => {
+app.post("/webhook", async (req, res, next) => {
   try {
     let reqBody = await req.body;
 
